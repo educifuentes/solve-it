ac5fa67d15ed40f1dadafa30528aa13bc5a63098
Fernando Aguilera Croasdaile <fa.croasdaile@gmail.com>
2021-12-11 11:18:54 -0600

    created files and connections

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :services_technicians, only: %i[new create]
   resources :categories, only: %i[show index]
-  resources :service, only: %i[index show]
+  resources :services, only: %i[index show]
 
   resources :orders, only: %i[index show new create] do
     resources :reviews, only: %i[new create]

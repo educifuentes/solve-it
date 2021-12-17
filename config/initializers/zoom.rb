0ba9c43b409f28dc4c25beeec23d13097f002b4a
Eduardo Cifuentes <edu.cifuentes@gmail.com>
2021-12-18 00:44:51 +0100

    zoom gem and config

---
+++ config/initializers/zoom.rb
@@ -0,0 +1,29 @@
+require 'zoom_rb'
+require 'json'
+# require_relative "/.env"
+
+Zoom.configure do |c|
+  c.api_key = "8uF__74nTTGxwCWddWwtgA"
+  c.api_secret = "2U7DjjI9LrtrfnXt7UUOlddOWzq4676AeJWA"
+  # c.api_key = ENV["ZOOM_US_API_KEY"]
+  # c.api_secret = ENV["ZOOM_US_API_SECRET"]
+end
+
+# For Testing
+# Starting a new zoom client
+# zoom_client = Zoom.new
+
+# # Getting the user list
+# user_list = zoom_client.user_list
+
+# # Grabing the desired user's id
+# zoom_user_id = user_list['users'][0]['id']
+# # puts "Zoom.us user_id = #{zoom_user_id}"
+
+# # Creating a meeting with the selected user's id
+# zoom_client.meeting_create(user_id: zoom_user_id, start_time: DateTime.now)
+
+# # Getting the list of meetings that belong to a certain user
+# pp zoom_client.meeting_list(user_id: zoom_user_id)["meetings"].last["join_url"]
+
+# # pp zoom_client.meeting_list(user_id: zoom_user_id)["meetings"].last

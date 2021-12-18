e1b48b1fc07c79d6bdc110e4fab2bb60eb36aa51
Eduardo Cifuentes <edu.cifuentes@gmail.com>
2021-12-18 17:05:45 +0100

    fix autocomplete feature in categoris index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get "/pages/autocomplete", to: "pages#autocomplete", as: :autocomplete
+  get "/categories/autocomplete", to: "categories#autocomplete", as: :autocomplete
   get "/search", to: "services#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :services_technicians, only: %i[new create index show]

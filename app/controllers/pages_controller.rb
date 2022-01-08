e67433aa2e84f91d1692de799fa8301f958fa2bc
Julio Guerra <juliowar33@gmail.com>
2022-01-08 17:04:50 -0500

    Technician Redirects to Dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
+    redirect_to dashboard_path if current_user.present? && current_user.role == "technician"
     @categories = Category.all
   end
 
